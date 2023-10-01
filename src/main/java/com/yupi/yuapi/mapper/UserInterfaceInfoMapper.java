aab7b967eed213da242a7da9f1cf014516b4f966
leikooo <108205879+lieeew@users.noreply.github.com>
2023-10-01 22:03:48 +0800

    把通用 entity 抽取到 commen 项目之中，把这 API-backend 项目修改成使用 commen 包里面的 entity

---
+++ src/main/java/com/yupi/yuapi/mapper/UserInterfaceInfoMapper.java
@@ -0,0 +1,19 @@
+package com.yupi.yuapi.mapper;
+
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.yuapicommen.model.entity.UserInterfaceInfo;
+
+/**
+* @author liang
+* @description 针对表【user_interface_info(用户调用接口关系)】的数据库操作Mapper
+* @createDate 2023-09-26 20:39:07
+* @Entity UserInterfaceInfo
+*/
+public interface UserInterfaceInfoMapper extends BaseMapper<UserInterfaceInfo> {
+
+}
+
+
+
+
